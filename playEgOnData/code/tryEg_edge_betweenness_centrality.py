abc79be712aac86e30fb914ef0bdc019e3a590f1
Gudauu <gudautd@gmail.com>
2023-02-22 12:02:11 +0800

    edge betweeness centrality on all

--- playEgOnData/code/tryEg_edge_betweenness_centrality.py
+++ playEgOnData/code/tryEg_edge_betweenness_centrality.py
@@ -4,7 +4,8 @@ from include import *
 if __name__ == '__main__':
     DEBUG = True
     # versions = ['19980101','19980201','19980301','19980401']
-    for fn in listFileName_1998:
+    list_temp = list(dictInfile.values()) + listFileName_1998
+    for fn in list_temp:
         G = buildAsRelGraph_nx(fn, flag_directed = True)
 
         dict_edge_betweenness_centrality = nx.edge_betweenness_centrality(G)
@@ -13,7 +14,7 @@ if __name__ == '__main__':
         ofile = open('playEgOnData/results/'+version+'/edge_betweenness_centrality','w')
 
         for edge, value in dict_edge_betweenness_centrality.items():
-            ofile.write(f"{edge}:{value}")
+            ofile.write(f"{edge}:{value}\n")
 
         ofile.close()
 
