549b6e1b73675572460c823d08ba058b60025f1d
Gudauu <gudautd@gmail.com>
2023-02-22 11:42:36 +0800

    ignore __pycache__

--- playEgOnData/code/tryEg_edge_betweenness_centrality.py
+++ playEgOnData/code/tryEg_edge_betweenness_centrality.py
@@ -4,7 +4,7 @@ from include import *
 if __name__ == '__main__':
     DEBUG = True
     # versions = ['19980101','19980201','19980301','19980401']
-    for fn in dictInfile.values():
+    for fn in listFileName_1998:
         G = buildAsRelGraph_nx(fn, flag_directed = True)
 
         dict_edge_betweenness_centrality = nx.edge_betweenness_centrality(G)
