0ab6eb2419eb35a00e2bf24618bbb8494b6f04c4
Gudauu <gudautd@gmail.com>
2023-03-12 17:27:12 +0800

    explored till 25462

--- playEgOnData/code/track_as_merge.py
+++ playEgOnData/code/track_as_merge.py
@@ -49,7 +49,7 @@ def track_asn(asn:int,version_first:str,version_second:str,thresh:float):  # 776
 
 
 if __name__ == '__main__':
-    # track_asn(25462,'20080101','20090101',float(3/7))   # new node 9002 by the "same" company take up its role
+    # track_asn(25462,'20080101','20090101',float(3/7))   # new node 9002 by the "same" company take up its role US->US
     # original length of neighbors: 646
 
     # 9002: None
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     # len common: 237   len covered: 600
 
     # track_asn(22822,'20100101','20110101',float(3/7))  # no other node seems to be taking up its role. neighbors before: 776
-    # track_asn(1,'20030101','20040101',float(2/7))  # node 3356 grow a lot while 1 reduce from 663 to 161
+    # track_asn(1,'20030101','20040101',float(2/7))  # node 3356 grow a lot while 1 reduce from 663 to 161 US->US
     # original length of neighbors: 621
 
     # 3356:558 | 74
@@ -77,7 +77,7 @@ if __name__ == '__main__':
 
     # 25462:501 | 284
     # len common: 119   len covered: 478
-    track_asn(14840,'20220101','20230101',float(2/7))  # 9554 -> 759
+    # track_asn(14840,'20220101','20230101',float(2/7))  # 9554 -> 759, Brazil -> France
     # original length of neighbors: 4895
 
     # 35280:956 | 651
@@ -85,6 +85,60 @@ if __name__ == '__main__':
 
     # 35280:4339 | 2371
     # len common: 1144   len covered: 4018
+
+    # track_asn(8359,'20150101','20160101',float(3/7))  # 2380 -> 1140, Russian -> Singapore
+    # original length of neighbors: 1367
+
+    # 24482:97 | 72
+    # len common: 50   len covered: 1062
+
+    # 24482:2705 | 911
+    # len common: 584   len covered: 1038
+
+    # track_asn(8220,'20060101','20070101',float(3/7))  # 1133 -> 653, no specific change
+    # track_asn(39120,'20200101','20210101',float(3/7))  # 7563 -> 6407, Italy -> Hungarian
+    # original length of neighbors: 3781
+
+    # 39533:543 | 474
+    # len common: 366   len covered: 3770
+
+    # 39533:3698 | 2410
+    # len common: 1293   len covered: 3456
+
+    # track_asn(49605,'20200101','20210101',float(2/7))  # 4891 -> 777, no specific change
+    # track_asn(31500,'20150101','20160101',float(2/7))  # 2363 -> 243, Antigua and Barbuda -> Singapore
+    # original length of neighbors: 1205
+
+    # 24482:97 | 70
+    # len common: 29   len covered: 1188
+
+    # 24482:2705 | 1025
+    # len common: 449   len covered: 1116
+    # track_asn(28260,'20190101','20200101',float(3/7))  # 4772 -> 3056, Brazil -> Brazil
+    # original length of neighbors: 2418
+
+    # 267613:160 | 126
+    # len common: 4   len covered: 2337
+
+    # 267613:2853 | 1779
+    # len common: 1358   len covered: 2137
+
+    track_asn(25462,'20080101','20090101',float(3/7))  # 1174 -> None
+    # original length of neighbors: 646
+
+    # 9002: None
+    # 13030:133 | 66
+    # len common: 39   len covered: 512
+
+    # 9002:1031 | 570
+    # 13030:928 | 425
+    # len common: 237   len covered: 600
+
+
+
+
+
+
 
 
     
