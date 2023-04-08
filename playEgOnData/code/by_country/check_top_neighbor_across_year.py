b5d26bc22bec0b95cf433286a3acbaaf3838128e
Gudauu <gudautd@gmail.com>
2023-04-08 22:25:46 +0800

    calc ratio of first and second, get list of highy 'domestic' country

--- playEgOnData/code/by_country/check_top_neighbor_across_year.py
+++ playEgOnData/code/by_country/check_top_neighbor_across_year.py
@@ -36,11 +36,121 @@ def check_top_neighbor_across_year(country:str,year_start:int,year_end:int,month
     
     ofile.close()
 
+# countries with at least one time top AS not self:
+# {"AT","HU","LU","FI","IE","AE","CZ","EU","SI","MY","HK","BM","FR","SE","ZA","BE","DK","NO","ZZ","GB","SG","CY","PT","PR"}
+# AT: Austria
+# HU: Hungary
+# LU: Luxembourg
+# FI: Finland
+# IE: Ireland
+# AE: United Arab Emirates
+# CZ: Czech Republic
+# EU: European Union
+# SI: Slovenia
+# MY: Malaysia
+# HK: Hong Kong
+# BM: Bermuda
+# FR: France
+# SE: Sweden
+# ZA: South Africa
+# BE: Belgium
+# DK: Denmark
+# NO: Norway
+# ZZ: Unknown or invalid country code
+# GB: United Kingdom
+# SG: Singapore
+# CY: Cyprus
+# PT: Portugal
+# PR: Puerto Rico
+def check_top_neighbor_not_self_across_year(country_list:list,year_start:int,year_end:int,month:str='0101'):
+    set_countries = set()
+    ofile = open('playEgOnData/results/2001-2023/top_neighb_not_self','w')
+    for year in range(year_start,year_end+1):
+        set_countries_not_self = set()
+        for country in country_list:
+            ifile = open(f"playEgOnData/results/{year}{month}/by_country/{country}/neighbors_count_by_country",'r')
+            for line in ifile:
+                if line[:2] != country:
+                    set_countries_not_self.add((country,line[:2]))  # strip the last '\n'
+                    set_countries.add(country)
+                break
+        ofile.write(f'{year}\n')
+        for cc in set_countries_not_self:
+            ofile.write(f'{cc} ')
+        ofile.write('\n')
+    # print("summary\n")
+    # for cc in set_countries:
+    #     print(f'{cc}',end=' ')
+
+
+# record the min ratio, max ratio and average ratio of first degree / second degree of each country in list across year range.
+def calc_ratio_top_second_across_year(country_list:list,year_start:int,year_end:int,month:str='0101'):
+    list_country_not_top = {"AT","HU","LU","FI","IE","AE","CZ","EU","SI","MY","HK","BM","FR","SE","ZA","BE","DK","NO","ZZ","GB","SG","CY","PT","PR"}
+    ofile = open('playEgOnData/results/2001-2023/country_degree_ratio_top_second','w')
+    # output format:
+    # CN : average_ratio min_ratio(year)  max_ratio(year) is_not_top
+    list_country_large_ratio = []
+    for country in country_list:
+        ofile.write(f'{country}: ')
+        sum_ratio = 0
+        min_ratio_year = (99999,-1)
+        max_ratio_year = (-1,-1)
+        for year in range(year_start,year_end+1):
+            ifile = open(f"playEgOnData/results/{year}{month}/by_country/{country}/neighbors_count_by_country",'r')
+            degree_top = -1
+            degree_second = -1
+            # only read first two lines
+            for line in ifile:
+                if degree_top == -1:
+                    degree_top = int((line[:-1].split(':'))[1])  # CN:900
+                else:
+                    degree_second = int((line[:-1].split(':'))[1]) 
+                    break
+            ratio = float(degree_top)/degree_second
+            sum_ratio += ratio
+            if ratio < min_ratio_year[0]:
+                min_ratio_year = (ratio,year)
+            if ratio > max_ratio_year[0]:
+                max_ratio_year = (ratio,year)
+        length = year_end - year_start + 1 
+        format_width = 5
+        min_ratio_str = f'{round(min_ratio_year[0],1)}({min_ratio_year[1]})'
+        max_ratio_str = f'{round(max_ratio_year[0],1)}({max_ratio_year[1]})'
+        avg_ratio = round(sum_ratio/length,1)
+        if avg_ratio >= 10:
+            list_country_large_ratio.append(country)
+
+        ofile.write(f'{avg_ratio:<{format_width}}\
+            {min_ratio_str:<{format_width}}\
+            {max_ratio_str:<{format_width}}')
+        
+        if country in list_country_not_top:
+            ofile.write('   True\n')
+        else:
+            ofile.write('\n')
+    print(list_country_large_ratio)
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 if __name__ == '__main__':
     list_country = readList('dataCAIDA/ASN_lookup/filterd_3_neighbor_country_list') 
-    for country in list_country:
-        check_top_neighbor_across_year(country,2001,2023,'0101',10)
+    # check_top_neighbor_not_self_across_year(list_country,2001,2023)
+    calc_ratio_top_second_across_year(list_country,2001,2023)
 
         
 
