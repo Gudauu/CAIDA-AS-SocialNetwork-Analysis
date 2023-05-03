ef752f8e1b75130b1f7273f576281cc85d78551b
Gudauu <gudautd@gmail.com>
2023-05-04 00:06:15 +0800

    R: basic ASR trend & expo fit. Color is buggy

---
+++ playEgOnData/code/ASR_count_fit_formula.py
@@ -0,0 +1,72 @@
+from include import readList
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from icecream import ic
+
+# Define the exponential function
+def exponential_func(x, a, b, c):
+    return a * np.exp(-b * x) + c
+
+def calc_exponential_draw_pic(version:"0101"):
+    list_edge_size = []
+
+    for year in range(2000,2023+1):
+        ifile_name = f'playEgOnData/results/{year}0101/basic'
+        with open(ifile_name,'r') as ifile:
+            ilines = ifile.readlines()
+            node_size = int(ilines[0][:-1].split(': ')[1])
+            edge_size = int(ilines[1][:-1].split(': ')[1])
+            list_edge_size.append(edge_size)
+    data = np.array(list_edge_size)
+    # Define the x-axis values
+    x_values = np.arange(len(data))
+    # year_values is what's gonna show on pic
+    year_values = np.arange(2000,2000 + len(data))
+
+    outfile_name = f'playEgOnData/results/2000-2023/exponential_fit_ASR_count.png'
+    ofile_fit_data = open('report/R/basic/ASR_exponential_pred','w')
+
+    # ic(data,x_values)
+    # Fit the exponential function to the data
+    popt, pcov = curve_fit(exponential_func, x_values[:-3], data[:-3])
+    y_pred = exponential_func(x_values, *popt)
+
+    ofile_fit_data.write(",".join([str(int(y)) for y in list(y_pred)]))
+    ofile_fit_data.close()
+
+
+    # Print the fitted parameters
+    # print(popt)
+
+    # Plot the data and the fitted function
+    plt.plot(year_values, data, 'bo', label='data')
+    plt.plot(year_values, y_pred, 'r-', label='fit')
+
+    # Annotate the equation on the plot
+    equation = f'y = {popt[0]:.2f} * exp({-1*popt[1]:.2f} * x) + ({popt[2]:.2f})'
+    # Calculate and print the R-squared value
+    r2 = r2_score(data, y_pred)
+    # print(f"R-squared: {r2:.4f}")
+    annotation_R_square = f'R-square: {r2}'
+    plt.annotate(equation, xy=(0.05, 0.7), xycoords='axes fraction',fontsize=11)
+    plt.annotate(annotation_R_square, xy=(0.05, 0.6), xycoords='axes fraction',fontsize=10)
+
+    # Add legend and axis labels
+    plt.legend()
+    plt.xlabel('year')
+    plt.ylabel('ASR count')
+
+    # Save the figure
+    plt.savefig(outfile_name)
+    plt.clf()
+
+    # Show the plot
+    # plt.show()
+
+if __name__ == '__main__':
+    calc_exponential_draw_pic(version = "0101")
+
+
+
---
+++ report/R/basic/ASR_size_cross_year.R
@@ -0,0 +1,55 @@
+library(ggplot2)
+
+
+function_ASR_size_across_year <- function(version = "0101"){
+    # read the data from the file
+    years <- seq(2000, 2023, 1)
+    vec_size_node <- c()
+    vec_size_edge <- c()
+    for(year in years){
+        file_name <- paste0("./playEgOnData/results/", year, version, "/basic")
+        lines <- readLines(file_name)
+
+        line_node <- lines[1]
+        line_edge <- lines[2]
+        size_node <- sapply(strsplit(line_node, ": "), "[[", 2)#[1]
+        # print(size_node)
+        size_edge <- sapply(strsplit(line_edge, ": "), "[[", 2)
+        vec_size_node <- c(vec_size_node, size_node)
+        vec_size_edge <- c(vec_size_edge, size_edge)
+    }
+    vec_size_edge_expo <- scan(text =  readLines("./report/R/basic/ASR_exponential_pred")[1], sep = ",")
+
+    df_node <- data.frame(x = years, y = as.numeric(vec_size_node))
+    df_edge <- data.frame(x = years, y = as.numeric(vec_size_edge))
+    df_edge_fit <- data.frame(x = years, y = as.numeric(vec_size_edge_expo))
+
+
+    p <- ggplot() +
+    # nodes line
+    geom_line(data = df_node, aes(x = x, y = y, color = "red")) +
+    geom_point(data = df_node, aes(x = x, y = y, color = "red"), size = 2) +
+    geom_text(data = df_node, aes(x = x, y = y, label = y), vjust = -1, size = 2, color = "black") +
+    # edges line
+    geom_line(data = df_edge, aes(x = x, y = y, color = "blue")) +
+    geom_point(data = df_edge, aes(x = x, y = y, color = "blue"), size = 2) +
+    geom_text(data = df_edge, aes(x = x, y = y, label = y), vjust = -1, size = 2, color = "black") +
+    # edges expo fit line
+    geom_line(data = df_edge_fit, aes(x = x, y = y, color = "grey")) +
+    geom_point(data = df_edge_fit, aes(x = x, y = y, color = "grey"), size = 2) +
+    # geom_text(data = df_edge_fit, aes(x = x, y = y, label = y), vjust = -1, size = 2, color = "black") +
+    # tags, scale, label
+    # scale_color_manual(values = c("blue", "red","grey"), labels = c("AS count", "ASR count","ASR count predict")) +
+    scale_x_continuous(breaks = seq(2000, 2023, 2)) +
+    labs(x = "Year", y = "Total Size", title = "Size of ASes and ASRs") +
+    # hide title on color legend
+    guides(color = guide_legend(title = NULL))
+
+
+
+    ggsave("report/R/basic/result/size_of_ASes_and_ASRs.png",plot = p, width = 8, height = 6, dpi = 300, device = "png")
+
+}
+
+
+function_ASR_size_across_year("0101")
