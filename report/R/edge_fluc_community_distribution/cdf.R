9f4f9dc5aa27018c2e237c0cab4e214a5be51a3d
Gudauu <gudautd@gmail.com>
2023-05-06 21:58:45 +0800

    R:edge_fluc_community_distribution/cdf.R: debugging with gpt

--- report/R/edge_fluc_community_distribution/cdf.R
+++ report/R/edge_fluc_community_distribution/cdf.R
@@ -1,37 +1,39 @@
 library(ggplot2)
 
 
-function_degree_CDF <- function(year){
+function_community_rank_CDF <- function(year){
     version <- "0101"
-    # Read data from file
-    data <- readLines(paste0("playEgOnData/results/", year, version, "/degree_distribution"))
-    degrees <- rev(as.numeric(sapply(strsplit(data, ": "), "[[", 1)))
-    counts <- rev(as.numeric(sapply(strsplit(data, ": "), "[[", 2)))  
 
-    # degree: low to high
+    data <- readLines(paste0("playEgOnData/results/", year, version, "/added_ASR_community_distribution"))
+    data_df <- data.frame(do.call(rbind, strsplit(data, ":|,")))
 
-    # Calculate cumulative percentages
-    total_nodes <- sum(counts) 
-    cumulative_counts <- cumsum(counts)
-    cumulative_percentages <- 100 * cumulative_counts / total_nodes
 
-    df <- data.frame(x = degrees, y = cumulative_percentages)
+    data_df[,1:4] <- lapply(data_df[,1:4], as.integer)
+    data_df[,5] <- as.numeric(data_df[,5])
 
-    # Plot CDF using ggplot2
-    p <- ggplot(df, aes(x = x, y = y, color = "#EF5350")) +
-        geom_step() +
-        labs(x = "Degree", y = "Percentage",
-            title = paste0("Degree Distribution CDF ", year)) +
-        # hide title on color legend
-        guides(colour = FALSE)
 
-    # Save plot as PNG using ggsave
-    ggsave(paste0("report/R/degree_distribution_cdf/result/degree_distribution_cdf_", year, ".png"),
-        plot = p, width = 8, height = 6, dpi = 300)
+    cum_instances <- tapply(data_df[,3], data_df[,4], cumsum)
+    print(cum_instances)
+
+    cum_instances <- unlist(cum_instances)
+    # print(cum_instances)
+    total_instances <- sum(data_df[,3])
+
+    # print(cum_counts)
+    cdf_values <-  100 * cumsum(cum_instances) / total_instances
+
+    # abline(h = 0.5, lty = 2)
+
+    ggplot(data = data.frame(Rank = names(cdf_values), CDF = cdf_values), aes(x = Rank, y = CDF)) +
+      geom_line() +
+      xlab("Rank") +
+      ylab("CDF")
+
+    # Step 8
+    ggsave(paste0("report/R/edge_fluc_community_distribution/result/added_ASR_community_rank_cdf_",year,version,".png"),
+        width = 6, height = 4, dpi = 300)
 }
 
-years <- seq(2000, 2023, 1)
-for (year in years){
-    function_degree_CDF(year)
-}
+# years <- seq(2000, 2023, 1)
+function_community_rank_CDF(2001)
 
